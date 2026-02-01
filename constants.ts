8f72e0a0926f9f89848269268274dc4c8857785f
Thomas Barros <41028866+Thomasdfoz@users.noreply.github.com>
2026-01-31 21:05:30 -0300

    Updates

--- constants.ts
+++
@@ -1,5 +0,0 @@
-import data from './resources/data_br.json';
-import { AppData } from './types';
-
-export const appData: AppData = data as AppData;
-export const { profile, skills, projects, contact } = appData;
